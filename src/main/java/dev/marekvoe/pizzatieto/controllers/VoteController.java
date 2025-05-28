9ad3766959742ab2d691409f501467a913a73a25
MarekVoe <marekhrachovec3@gmail.com>
2025-05-28 13:37:10 +0200

    Add ID generation for new pizzas and clean up VoteController imports

--- src/main/java/dev/marekvoe/pizzatieto/controllers/VoteController.java
+++ src/main/java/dev/marekvoe/pizzatieto/controllers/VoteController.java
@@ -1,6 +1,5 @@
 package dev.marekvoe.pizzatieto.controllers;
 
-import dev.marekvoe.pizzatieto.models.Pizza;
 import dev.marekvoe.pizzatieto.services.TokenService;
 import dev.marekvoe.pizzatieto.services.VoteService;
 import org.slf4j.Logger;
@@ -8,8 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-
-import static java.rmi.server.LogStream.log;
 
 @RestController
 @RequestMapping("/votes")
--- src/main/java/dev/marekvoe/pizzatieto/services/PizzaService.java
+++ src/main/java/dev/marekvoe/pizzatieto/services/PizzaService.java
@@ -1,3 +1,4 @@
+// src/main/java/dev/marekvoe/pizzatieto/services/PizzaService.java
 package dev.marekvoe.pizzatieto.services;
 
 import dev.marekvoe.pizzatieto.models.Pizza;
@@ -6,17 +7,20 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class PizzaService {
 
     private final List<Pizza> pizzas = new ArrayList<>();
+    private final AtomicInteger idCounter = new AtomicInteger(1);
 
     public List<Pizza> getAllPizzas() {
         return new ArrayList<>(pizzas);
     }
 
     public Pizza addPizza(Pizza pizza) {
+        pizza = new Pizza(idCounter.getAndIncrement(), pizza.getName(), pizza.getDescription(), pizza.getUpvotes());
         pizzas.add(pizza);
         return pizza;
     }
